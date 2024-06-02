d8158bf12a7f902dabb350855a34009e9e9b743e
assrat007 <asratadane169@gmail.com>
2024-06-02 14:06:51 +0300

    text color chnage

--- components/Service.tsx
+++ components/Service.tsx
@@ -5,15 +5,15 @@ import { service } from "@/lib/constant"
 function Service() {
     
   return (
-    <section className="my-6">
-        <h1 className='text-center font-medium text-2xl'>
-            Service i provide
+    <section className="my-14">
+        <h1 className='text-center font-bold text-2xl text-[#627254] dark:text-yellow-700 '>
+            What i Offer
         </h1>
         <div className="my-6 grid grid-cols-4  grid-flow-dense gap-2  w-full">
          {
          service.map((item,index)=>{
           return(
-         <ServiceCard/>
+         <ServiceCard Service={item}/>
           
           )
          })
