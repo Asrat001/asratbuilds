f3fa99fd79b2e010d8cc48e7d0c298abcec006ed
assrat007 <asratadane169@gmail.com>
2024-06-02 14:04:22 +0300

    Card Design Chenge

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,11 +1,12 @@
 import { emojis,rings } from "@/lib/constant";
 
 function ProjectCard() {
-    const { ring, bg } =
-					rings[Math.floor(Math.random() * rings.length)];
+    const { ring, bg } =rings[Math.floor(Math.random() * rings.length)];
+	console.log(ring)
+					
   return (
     <div className="relative group">
-							<div className=" border border-zinc-600 rounded-md p-5 space-y-3 bg-zinc-800 group-hover:translate-x-3 group-hover:translate-y-3 transition-all relative">
+							<div className=" border border-zinc-600 rounded-md p-5 space-y-3 bg-zinc-100 dark:bg-zinc-800  group-hover:translate-x-3 group-hover:translate-y-3 transition-all relative h-[350px]">
 								<div className="flex items-center gap-2">
 									{/* <Image
 										src={users?.avatar_url!}
@@ -38,7 +39,7 @@ function ProjectCard() {
 								</span>
 							</div>
 							<div
-								className={` -z-10 absolute top-0 right-0 translate-x-3 translate-y-3 w-full h-full ring-1  rounded-md ${ring} ${bg} bg-opacity-10`}
+								className={` -z-10 absolute top-0 right-0 translate-x-3 translate-y-3 w-full h-full ring-1  rounded-md ${rings[2]} ${bg}  bg-opacity-10`}
 							></div>
 						</div>
   )
